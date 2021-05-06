de33a430bb04f1d12f067deed903c2098a5c14bc
Animesh Kumar <animesh-kumar@pop-os.localdomain>
2021-05-06 15:57:54 +0530

    Add basic image processor

--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -1,4 +1,13 @@
+import cv2
+
 def handle_uploaded_file(f):
     with open('static_my_project/input.jpg', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    image_processor()
+
+
+
+def image_processor():
+    grayscale_img = cv2.imread('static_my_project/input.jpg', 0)
+    cv2.imwrite('static_my_project/output.jpg', grayscale_img)
